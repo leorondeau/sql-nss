65a5aea8257ab67333cc6e013bd728472b1897ac
Leo Rondeau <christopherleorondeau@gmail.com>
2021-06-07 17:01:26 -0500

    exercise 2

--- book-2/chap-12.sql
+++ book-2/chap-12.sql
@@ -17,6 +17,17 @@ join
 	on vma.vehicle_make_id = v.vehicle_make_id;
 
 --Create a view that shows the total number of employees for each employee type.
+
+CREATE view total_employees_per_type as
+select 
+	et.name,
+	count(e.employee_type_id) as total_employee_type
+from 
+	employeetypes et
+join employees e
+	on e.employee_type_id = et.employee_type_id
+group by
+	e.employee_type_id, et.name ; 
 --Create a view that lists all customers without exposing their emails, phone numbers and street address.
 --Create a view named sales2018 that shows the total number of sales for each sales type for the year 2018.
 --Create a view that shows the employee at each dealership with the most number of sales.
