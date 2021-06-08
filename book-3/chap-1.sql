29a1c1314645ee76d7368ab187fc88132263f2f8
Leo Rondeau <christopherleorondeau@gmail.com>
2021-06-08 16:36:11 -0500

    added exercise

--- book-3/chap-1.sql
+++ book-3/chap-1.sql
@@ -6,3 +6,10 @@
 update dealershipemployees
 set dealership_id = 20
 where employee_id = 9;
+
+--A Sales associate needs to update a sales record because her customer want to pay with a Mastercard instead of JCB. 
+--Update Customer, Ernestus Abeau Sales record which has an invoice number of 9086714242
+
+update sales
+set payment_method = 'mastercard'
+where invoice_number = '9086714242';
